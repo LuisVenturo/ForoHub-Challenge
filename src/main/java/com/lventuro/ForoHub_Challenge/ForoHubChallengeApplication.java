0f29e61f7ead4fbe01dc7a402ecf83492acb051f
LuisVenturo <venturomoralesluis@gmail.com>
2025-08-19 19:56:08 -0500

    Challenge ForoHub

---
+++ src/main/java/com/lventuro/ForoHub_Challenge/ForoHubChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.lventuro.ForoHub_Challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoHubChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoHubChallengeApplication.class, args);
+	}
+
+}
